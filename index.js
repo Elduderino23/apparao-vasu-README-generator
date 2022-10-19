4ede7c61939ccbc86085e0f321834c6bb4033c44
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-10-18 20:42:41 -0700

    add gitignore INPUT.MD and add code to index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+// const fs = require("fs") 
+// const pathfinder = require("path") 
+// fs.writeFile(pathfinder.join("C:\Users\xfile\bootcamp\README generator\apparao-vasu-README-generator\", userReadmeName+".md"), "Testresult", (event)=>{console.log("ERROR")})
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -11,3 +14,16 @@ function init() {}
 
 // Function call to initialize app
 init();
+inquirer.prompt([{
+    name: "q1confirm",
+    message: "Do you want a README.md like a professional?",
+    type: "confirm",
+
+}, {
+    name: "response", 
+    message: "Excellent, what name do you want to give your project?",
+    type: "input",
+
+}]).then(function(data){
+    console.log(data)
+})
