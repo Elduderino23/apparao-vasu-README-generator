5e26e6fb34ac719470e0ae2097afd4e562a4548f
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-10-20 17:17:37 -0700

    clean up code for index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require("fs") 
-// const pathfinder = require("path") 
-// fs.writeFile(pathfinder.join("C:\Users\xfile\bootcamp\README generator\apparao-vasu-README-generator\", userReadmeName+".md"), "Testresult", (event)=>{console.log("ERROR")})
+
 // TODO: Create an array of questions for user input
-const questions = ["Do you want a README.md like a professional", "what name do you want to give your project?", "do you want a Table of Contents?", "do you want a Description section?", "do you want a Installation section?", "do you want a Usage section?", "do you want a Contributing section?", "Excellent, do you want a Test section?", "do you want a License section?", "are you sure with your selection of license?", "Excellent, do you want a Question section?", "are you satisfied with your README choices?", "would you kindly wait for your README to generate?" ];
-var license = ["none","[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0", "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)", "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)", "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)", "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)", "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)", "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)", "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)", "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)", "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"]
+const questions = []
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFileSync(fileName, data);
